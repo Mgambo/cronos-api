08653c4d670559a6a31dc894f71492378a9a39f4
Eric Chan <ericchan@Erics-MacBook-Pro.local>
2025-05-29 11:17:20 +0800

    fix(core): typo in gitignore

--- dist/enums/errors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Errors = void 0;
-var Errors;
-(function (Errors) {
-    Errors["INTERNAL_SERVER_ERROR"] = "Internal Server Error";
-    Errors["INVALID_ADDRESS"] = "Invalid Address";
-})(Errors || (exports.Errors = Errors = {}));
--- dist/routes/health.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const healthRouter = (0, express_1.Router)();
-healthRouter.get("/healthz", (_, res) => {
-    res.send("OK");
-});
-exports.default = healthRouter;
